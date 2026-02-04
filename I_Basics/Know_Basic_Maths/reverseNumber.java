02b66049f2787ee4d8e703079aeb47db0b0e77ec
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-04 19:44:42 +0530

    Reverse a number

---
+++ I_Basics/Know_Basic_Maths/reverseNumber.java
@@ -0,0 +1,21 @@
+package I_Basics.Know_Basic_Maths;
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0, rem;
+
+        if (Integer.MIN_VALUE <= x && x <= Integer.MAX_VALUE) {
+            while (x != 0) {
+                rem = x % 10;
+                if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                    return 0;
+                }
+                rev = rev * 10 + rem;
+                x = x / 10;
+            }
+            return rev;
+        } else {
+            return 0;
+        }
+    }
+}
