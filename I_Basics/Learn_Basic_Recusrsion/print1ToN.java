48f952e1d7c878fbad610ee224b2e89d7b38180b
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-10 23:59:25 +0530

    Print 1 to N using recursions

---
+++ I_Basics/Learn_Basic_Recusrsion/print1ToN.java
@@ -0,0 +1,16 @@
+package I_Basics.Learn_Basic_Recusrsion;
+
+class solution {
+    public void printNumbers(int n) {
+        // base case
+        if (n == 0) {
+            return;
+        }
+
+        // recursive call
+        printNumbers(n - 1);
+
+        // print after recursion (to get increasing order)
+        System.out.println(n);
+    }
+}
