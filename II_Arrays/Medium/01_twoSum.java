5367b11be5bf5ca7d95444b349e908a2a46ac2ac
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-11 00:40:05 +0530

    Day8: Find two numbers in Array whose sum is equal to target (Two Sum Problem)

--- II_Arrays/Medium/01_twoSum.java
+++ II_Arrays/Medium/01_twoSum.java
@@ -1,5 +1,6 @@
 package II_Arrays.Medium;
 
+//Better and Optimal in this case(where we have to return indexes)
 import java.util.HashMap;
 
 class Solution {
@@ -19,3 +20,57 @@ class Solution {
         return new int[] { -1, -1 };
     }
 }
+
+//TC - O(N)
+//SC - O(1)
+
+
+
+//Brute Force
+/* class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+} */
+//TC - O(N^2)
+//SC - O(1)
+
+
+
+
+//Optimal only if we want to return yes or no and no indexes
+//Also note that two pointers approach work only when the array is sorted otherwise it does not work
+/* import java.util.Arrays;
+
+class Solution {
+    public boolean twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+
+            if (sum == target)
+                return true;
+            else if (sum < target)
+                left++;
+            else
+                right--;
+        }
+
+        return false;
+    }
+} */
+//TC - O(NLOGN) Because of sorting
+//sc - O(1) -> this is because indices are not returned in this, if indexes were to returned it will take O(N)
