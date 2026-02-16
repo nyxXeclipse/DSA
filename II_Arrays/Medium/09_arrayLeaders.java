c1a2a65c909c1c4f071fa34d21d91a2255405f62
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-16 20:22:00 +0530

    Day12: Leaders in an Array

--- II_Arrays/Medium/09_arrayLeaders.java
+++ II_Arrays/Medium/09_arrayLeaders.java
@@ -4,21 +4,53 @@ import java.util.ArrayList;
 import java.util.Collections;
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
-
-        ArrayList<Integer> res = new ArrayList<>();
-
-        int maxRight = arr[arr.length - 1];
-        res.add(maxRight); // last element is always a leader
-
-        for (int i = arr.length - 2; i >= 0; i--) {
-            if (arr[i] >= maxRight) {
-                maxRight = arr[i];
-                res.add(arr[i]);
+        // code here
+        int max = Integer.MIN_VALUE;
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] >= max) {
+                max = arr[i];
+                result.add(arr[i]);
             }
         }
-
-        // leaders were added from right to left, reverse result
-        Collections.reverse(res);
-        return res;
+        Collections.reverse(result);
+        return result;
     }
 }
+//TC - O(N)
+//SC - O(N)
+
+
+
+
+//Only if the return type is array
+/* class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        int max  = Integer.MIN_VALUE;
+        int[] temp = new int[arr.length];
+        int k=0;
+        for(int i=arr.length-1; i>=0; i--)
+        {
+            if(arr[i]>max)
+            {
+                max = arr[i];
+                temp[k] = arr[i];
+                k++;
+            }
+        }
+        reverse(temp,0,k-1);
+    }
+    
+    static void reverse(int[] arr, int start, int end)
+    {
+        while(start<end)
+        {
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+    }
+} */
