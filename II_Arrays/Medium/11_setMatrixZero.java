418bd68eeaccba986a5e454422038164de807499
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-17 23:57:10 +0530

    Day13: Set Matrix to zero

---
+++ II_Arrays/Medium/11_setMatrixZero.java
@@ -0,0 +1,29 @@
+package II_Arrays.Medium;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+        if (matrix.length == 0) {
+            return;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] == true || cols[j] == true) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
