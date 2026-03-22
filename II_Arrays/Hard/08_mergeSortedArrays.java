7b1f3a7d82a2c9794b9719cc78af62becd8d07d9
dishitaagupta <dishitagupta2003@gmail.com>
2026-03-23 01:26:25 +0530

    Day20: Merge two sorted arrays without extra space

--- II_Arrays/Hard/08_mergeSortedArrays.java
+++ II_Arrays/Hard/08_mergeSortedArrays.java
@@ -1,3 +1,58 @@
+/* //Brute
+import java.util.Arrays;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // copy nums2 into nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+
+        // sort nums1
+        Arrays.sort(nums1);
+    }
+}
+//TC - O((m + n) log(m + n))
+//SC - O(1)
+
+
+
+
+//Better
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] temp = new int[m + n];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp[k++] = nums1[i++];
+            } else {
+                temp[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            temp[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            temp[k++] = nums2[j++];
+        }
+
+        // copy back
+        for (int x = 0; x < m + n; x++) {
+            nums1[x] = temp[x];
+        }
+    }
+}
+//TC - O(m + n)
+//SC - O(m + n)
+
+
+
+
 //Optimal
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -37,3 +92,8 @@ class Solution {
 }
 //TC - O(m+n)
 //SC - O(1)
+
+
+
+
+ */
