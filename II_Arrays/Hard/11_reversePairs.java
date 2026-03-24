cf0a11c183bffb588b486ab36b677277b6383435
dishitaagupta <dishitagupta2003@gmail.com>
2026-03-25 01:10:51 +0530

    Day21: Reverse Pairs

--- II_Arrays/Hard/11_reversePairs.java
+++ II_Arrays/Hard/11_reversePairs.java
@@ -1,4 +1,4 @@
-//Brute Force
+/* //Brute Force
 class Solution {
     public int reversePairs(int[] nums) {
         int n = nums.length;
@@ -18,61 +18,71 @@ class Solution {
         return count;
     }
 }
+//TC - O(n²)
+//SC - O(1)
 
 
 
 //Optimal
 class Solution {
-    public int mergesort(int[] nums, int left, int right) {
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    public int mergeSort(int[] nums, int left, int right) {
         int count = 0;
-        if (left >= right)
-            return count;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            count = count + mergeSort(nums, left, mid);
+            count = count + mergeSort(nums, mid + 1, right);
+            count = count + countPairs(nums, left, mid, right);
+            merge(nums, left, mid, right);
+        }
+        return count;
+    }
 
-        int mid = left + (right - left) / 2;
-
-        count += mergesort(nums, left, mid);
-        count += mergesort(nums, mid + 1, right);
-
+    public int countPairs(int[] nums, int left, int mid, int right) {
+        int count = 0;
         int j = mid + 1;
+
         for (int i = left; i <= mid; i++) {
             while (j <= right && (long) nums[i] > 2L * nums[j]) {
                 j++;
             }
-            count += j - (mid + 1);
+            count += (j - (mid + 1));
         }
-
-        // optimization
-        if (nums[mid] <= nums[mid + 1])
-            return count;
-
-        merge(nums, left, right, mid);
 
         return count;
     }
 
-    public void merge(int[] nums, int left, int right, int mid) {
-        int[] temp = new int[right - left + 1];
+    public void merge(int[] nums, int left, int mid, int right) {
         int i = left, j = mid + 1, k = 0;
-
+        int temp[] = new int[right - left + 1];
         while (i <= mid && j <= right) {
             if (nums[i] <= nums[j]) {
-                temp[k++] = nums[i++];
+                temp[k] = nums[i];
+                i++;
+                k++;
             } else {
-                temp[k++] = nums[j++];
+                temp[k] = nums[j];
+                j++;
+                k++;
             }
         }
-
-        while (i <= mid)
-            temp[k++] = nums[i++];
-        while (j <= right)
-            temp[k++] = nums[j++];
-
-        for (i = left, k = 0; k < temp.length; i++, k++) {
-            nums[i] = temp[k];
+        while (i <= mid) {
+            temp[k] = nums[i];
+            i++;
+            k++;
+        }
+        while (j <= right) {
+            temp[k] = nums[j];
+            j++;
+            k++;
+        }
+        for (int p = 0; p < temp.length; p++) {
+            nums[left + p] = temp[p];
         }
     }
-
-    public int reversePairs(int[] nums) {
-        return mergesort(nums, 0, nums.length - 1);
-    }
 }
+//TC - O(n log n)
+//SC - O(n) */
