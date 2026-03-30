694917965317ebeff50b186f6c72c15cb7d15fa2
dishitaagupta <dishitagupta2003@gmail.com>
2026-03-30 21:55:00 +0530

    Day17: 4Sum

--- II_Arrays/Hard/04_fourSum.java
+++ II_Arrays/Hard/04_fourSum.java
@@ -1,167 +1,161 @@
 //package II_Arrays.Hard;
-/* //Brute
-class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 0; i < n - 3; i++) {
-            for (int j = i + 1; j < n - 2; j++) {
-                for (int k = j + 1; k < n - 1; k++) {
-                    for (int l = k + 1; l < n; l++) {
-                        int sum = nums[i] + nums[j] + nums[k] + nums[l];
-                        if (sum == target) {
-                            List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
-                            if (!result.contains(quad)) {
-                                result.add(quad);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return result;
-    }
-}
-
-
-
-
-//Better
-class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        Arrays.sort(nums);
-        long sum = 0;
-        for (int i = 0; i < n - 3; i++) {
-            for (int j = i + 1; j < n - 2; j++) {
-                int left = j + 1;
-                int right = n - 1;
-                while (left < right) {
-                    sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-                    if (sum < target) {
-                        left++;
-                    } else if (sum > target) {
-                        right--;
-                    } else {
-                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[left], nums[right]);
-                        if (!result.contains(quad)) {
-                            result.add(quad);
-                        }
-                        left++;
-                        right--;
-                    }
-                }
-            }
-        }
-        return result;
-    }
-}
-
-
-
-
-//Optimal
-// import java.util.*;
-// class Solution {
-// public List<List<Integer>> fourSum(int[] nums, int target) {
-
-// List<List<Integer>> result = new ArrayList<>();
-// int n = nums.length;
-// //Edge case
-// if (n < 4) return result;
-
-// Arrays.sort(nums);
-
-// for (int i = 0; i < n - 3; i++) {
-
-// // skip duplicate i
-// if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-// for (int j = i + 1; j < n - 2; j++) {
-
-// // skip duplicates for j
-// if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-// long remainingTarget = (long) target - nums[i] - nums[j];
-
-// int left = j + 1;
-// int right = n - 1;
-// // two pointer search
-
-// while (left < right) {
-
-// long sum = nums[left] + nums[right];
-
-// if (sum < remainingTarget) {
-// left++;
-// }
-// else if (sum > remainingTarget) {
-// right--;
-// }
-// else {
-// result.add(Arrays.asList(
-// nums[i], nums[j], nums[left], nums[right]
-// ));
-
-// // skip duplicates for left & right
-// while (left < right && nums[left] == nums[left + 1]) left++;
-// while (left < right && nums[right] == nums[right - 1]) right--;
-
-// left++;
-// right--;
-// }
-// }
-// }
-// }
-// return result;
-// }
-// }
-import java.util.*;
-
-class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        // Edge Case
-        if (n < 4)
-            return result;
-        Arrays.sort(nums);
-
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
-                long remainingTarget = (long) target - nums[i] - nums[j];
-                int left = j + 1;
-                int right = n - 1;
-
-                while (left < right) {
-                    long sum = nums[left] + nums[right];
-                    if (sum < remainingTarget) {
-                        left++;
-                    } else if (sum > remainingTarget) {
-                        right--;
-                    } else {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        // skip duplicate for left and right
-                        while (left < right && nums[left] == nums[left + 1])
-                            left++;
-                        while (left < right && nums[right] == nums[right - 1])
-                            right--;
-
-                        left++;
-                        right--;
-                    }
-                }
-
-            }
-        }
-        return result;
-    }
-}
+/*
+ * //Brute
+ * class Solution {
+ * public List<List<Integer>> fourSum(int[] nums, int target) {
+ * List<List<Integer>> result = new ArrayList<>();
+ * Arrays.sort(nums);
+ * int n = nums.length;
+ * for (int i = 0; i < n; i++) {
+ * for (int j = i + 1; j < n; j++) {
+ * for (int k = j + 1; k < n; k++) {
+ * for (int l = k + 1; l < n; l++) {
+ * int sum = nums[i] + nums[j] + nums[k] + nums[l];
+ * if (sum == target) {
+ * List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+ * if (!result.contains(quad)) {
+ * result.add(quad);
+ * }
+ * }
+ * }
+ * }
+ * }
+ * }
+ * return result;
+ * }
+ * }
+ * 
+ * ALTERNATE BRUTE USING HASHSET
+ * import java.util.*;
+ * 
+ * class Solution {
+ * public List<List<Integer>> fourSum(int[] nums, int target) {
+ * Set<List<Integer>> set = new HashSet<>();
+ * int n = nums.length;
+ * 
+ * for (int i = 0; i < n - 3; i++) {
+ * for (int j = i + 1; j < n - 2; j++) {
+ * for (int k = j + 1; k < n - 1; k++) {
+ * for (int l = k + 1; l < n; l++) {
+ * 
+ * long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+ * 
+ * if (sum == target) {
+ * List<Integer> quad = Arrays.asList(
+ * nums[i], nums[j], nums[k], nums[l]
+ * );
+ * 
+ * Collections.sort(quad); // avoid duplicates
+ * set.add(quad);
+ * }
+ * }
+ * }
+ * }
+ * }
+ * 
+ * return new ArrayList<>(set);
+ * }
+ * }
+ * //TC - O(N^4)
+ * //SC - O(no. of quads) * 2
+ * 
+ * 
+ * 
+ * 
+ * //Better
+ * import java.util.*;
+ * 
+ * class Solution {
+ * public List<List<Integer>> fourSum(int[] nums, int target) {
+ * Set<List<Integer>> res = new HashSet<>();
+ * int n = nums.length;
+ * 
+ * for (int i = 0; i < n; i++) {
+ * for (int j = i + 1; j < n; j++) {
+ * 
+ * Set<Long> seen = new HashSet<>();
+ * 
+ * for (int k = j + 1; k < n; k++) {
+ * 
+ * long fourth = (long) target - nums[i] - nums[j] - nums[k];
+ * 
+ * if (seen.contains(fourth)) {
+ * List<Integer> quad = Arrays.asList(
+ * nums[i], nums[j], nums[k], (int) fourth
+ * );
+ * 
+ * Collections.sort(quad); // ensure uniqueness
+ * res.add(quad);
+ * }
+ * 
+ * seen.add((long) nums[k]);
+ * }
+ * }
+ * }
+ * 
+ * return new ArrayList<>(res);
+ * }
+ * }
+ * //TC - O(n³) * logm
+ * //SC - O(N) + O(quads)*2
+ * 
+ * 
+ * 
+ * 
+ * //Optimal
+ * import java.util.*;
+ * 
+ * class Solution {
+ * public List<List<Integer>> fourSum(int[] nums, int target) {
+ * List<List<Integer>> result = new ArrayList<>();
+ * int n = nums.length;
+ * 
+ * if (n < 4) return result;
+ * 
+ * Arrays.sort(nums);
+ * 
+ * for (int i = 0; i < n - 3; i++) {
+ * 
+ * // skip duplicate i
+ * if (i > 0 && nums[i] == nums[i - 1]) continue;
+ * 
+ * for (int j = i + 1; j < n - 2; j++) {
+ * 
+ * // skip duplicate j
+ * if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+ * 
+ * int left = j + 1;
+ * int right = n - 1;
+ * 
+ * while (left < right) {
+ * 
+ * long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+ * 
+ * if (sum < target) {
+ * left++;
+ * }
+ * else if (sum > target) {
+ * right--;
+ * }
+ * else {
+ * result.add(Arrays.asList(
+ * nums[i], nums[j], nums[left], nums[right]
+ * ));
+ * 
+ * // skip duplicates for left & right
+ * while (left < right && nums[left] == nums[left + 1]) left++;
+ * while (left < right && nums[right] == nums[right - 1]) right--;
+ * 
+ * left++;
+ * right--;
+ * }
+ * }
+ * }
+ * }
+ * 
+ * return result;
+ * }
+ * }
+ * TC - O(N^3) * 
+ * SC - O(no. of quads)
  */
