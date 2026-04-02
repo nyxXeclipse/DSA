8ce92af568257f7ed1f34321b9d0230caf103b17
dishitaagupta <dishitagupta2003@gmail.com>
2026-04-02 23:24:59 +0530

    Day25: Search Insert Position

--- III_BinarySearch/BS_On_1D_Arrays/04_insertPosition.java
+++ III_BinarySearch/BS_On_1D_Arrays/04_insertPosition.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
     public int searchInsert(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
 
@@ -18,3 +18,27 @@ class Solution {
     }
 }
 //TC - O(log n)
+
+
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length; // default insert at end
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
+//Similar to LB problem */
