f441e62e71365bbc60f559a51efa13993f0d35bb
dishitaagupta <dishitagupta2003@gmail.com>
2026-04-03 17:08:49 +0530

    Day25: Implement Upper Bound

--- III_BinarySearch/BS_On_1D_Arrays/03_implementUB.java
+++ III_BinarySearch/BS_On_1D_Arrays/03_implementUB.java
@@ -23,32 +23,3 @@
 
 
 
-/* GFG
-import java.util.Arrays;
-
-class Solution {
-    public int[] getFloorAndCeil(int x, int[] arr) {
-        
-        Arrays.sort(arr);
-        
-        int floor = -1, ceil = -1;
-        int low = 0, high = arr.length - 1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (arr[mid] == x) {
-                return new int[]{x, x}; 
-            }
-            else if (arr[mid] < x) {
-                floor = arr[mid];
-                low = mid + 1;
-            } else {
-                ceil = arr[mid];
-                high = mid - 1;
-            }
-        }
-        
-        return new int[]{floor, ceil};
-    }
-} */
