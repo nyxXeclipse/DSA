ec46b1eeb7e21090fe30c1adabd87c8158d07abc
dishitaagupta <dishitagupta2003@gmail.com>
2026-04-07 13:49:06 +0530

    Day29: Find out how many times has an array been rotated

--- III_BinarySearch/BS_On_1D_Arrays/11_findKthRotation.java
+++ III_BinarySearch/BS_On_1D_Arrays/11_findKthRotation.java
@@ -1,3 +1,42 @@
+//Using the concept of min in arr
+class Solution {
+    public int findKRotation(int arr[]) {
+        // Code here
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int idx = -1;
+        int min = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < min) {
+                    min = arr[low];
+                    idx = low;
+                }
+            }
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] < min) {
+                    min = arr[low];
+                    idx = low;
+                }
+                low = mid + 1;
+            } else {
+                if (arr[mid] < min) {
+                    min = arr[mid];
+                    idx = mid;
+                }
+                high = mid - 1;
+            }
+        }
+        return idx;
+    }
+}
+//TC - O(log n)
+//SC - O(1)
+
+
+
+
 class Solution {
     public int findKRotation(int arr[]) {
         int low = 0, high = arr.length - 1;
@@ -19,3 +58,7 @@ class Solution {
 }
 // TC - O(log n)
 // SC - O(1)
+
+
+
+
