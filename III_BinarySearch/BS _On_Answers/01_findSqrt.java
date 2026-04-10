ec33c6459b858345fe6305f9711049c9c198fad0
dishitaagupta <dishitagupta2003@gmail.com>
2026-04-10 16:23:09 +0530

    Day31: Find square root of a number in log n

--- III_BinarySearch/BS _On_Answers/01_findSqrt.java
+++ III_BinarySearch/BS _On_Answers/01_findSqrt.java
@@ -1,10 +1,26 @@
+/* class Solution {
+    public int mySqrt(int x) {
+        int low = 1, high = x;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long sq = (long) mid * mid;
+            if (sq <= x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+}
+//TC - O(log n)
+
+
+
+
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) {
-            return x;
-        }
-
-        int low = 1, high = x / 2;
+        int low = 1, high = x;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
@@ -18,4 +34,4 @@ class Solution {
         return high;
     }
 }
-//TC - O(log n)
+//TC - O(log n) */
