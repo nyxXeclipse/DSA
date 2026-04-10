73a1d78e3ae8e43ab967978b25f496992e07995d
dishitaagupta <dishitagupta2003@gmail.com>
2026-04-10 16:22:36 +0530

    Day30: Single element in a Sorted Array

--- III_BinarySearch/BS_On_1D_Arrays/12_singleElementInSortedArr.java
+++ III_BinarySearch/BS_On_1D_Arrays/12_singleElementInSortedArr.java
@@ -1,3 +1,60 @@
+/* //Brute Force
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+        // check first element
+        if (n == 1 || nums[0] != nums[1]) {
+            return nums[0];
+        }
+
+        // check last element
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
+        }
+
+        // check middle elements
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+
+        return -1; // should never happen
+    }
+}
+//TC - O(n)
+
+
+
+
+//Optimal - BS
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+                return nums[mid];
+            if (mid % 2 == 1 && nums[mid] == nums[mid - 1] || mid % 2 == 0 && nums[mid] == nums[mid + 1])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
+
+
+
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
@@ -17,4 +74,4 @@ class Solution {
     }
 }
 //TC - O(log n)
-//SC - O(1)
+//SC - O(1) */
