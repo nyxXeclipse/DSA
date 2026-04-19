18f41c4038c8976a6d919e81940128834eb70c88
nyxXeclipse <dishitagupta2003@gmail.com>
2026-04-19 20:45:34 +0530

    Day35: Kth Missing Positive Number

--- III_BinarySearch/BS _On_Answers/07_kthMissingPosNum.java
+++ III_BinarySearch/BS _On_Answers/07_kthMissingPosNum.java
@@ -1,3 +1,27 @@
+//Brute Force
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int current = 1;
+        int i = 0;
+        while (k > 0) {
+            if (i < arr.length && arr[i] == current) {
+                i++;
+            } else {
+                k--;
+                if (k == 0)
+                    return current;
+            }
+            current++;
+        }
+        return current;
+    }
+}
+//TC - O(n + k)
+
+
+
+
+//Optimal
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         int low = 0, high = arr.length - 1;
